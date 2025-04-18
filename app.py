91f59758edb1ef42be64f1e7b3f004881cb19b8a
Chillaxnitish <nnitish448@gmail.com>
2025-04-18 20:16:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, request, redirect, url_for, session, jsonify
 import json
 
@@ -47,4 +48,6 @@ def logout():
     return redirect(url_for('login'))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Dynamically use the port provided by Render
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
